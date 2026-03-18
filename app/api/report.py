eb43d63a87a98f2283a9442b666d121479b964f2
egor-popov <egor_popov3@mail.ru>
2026-03-18 20:16:12 +0300

    Added validator for file

--- app/api/report.py
+++ app/api/report.py
@@ -3,6 +3,7 @@ from fastapi.responses import FileResponse
 from app.core.config import settings
 from app.services.analyzer import analyze_file
 from app.services.excel_writer import create_excel_report
+from app.services.validator import validate_file
 import aiofiles
 import os
 
@@ -17,12 +18,7 @@ async def process_file(
     file: UploadFile = File(...),
     background_tasks: BackgroundTasks = None
     ):
-    file_size_mb = file.size / (1024*1024) if file.size else 0
-    if file_size_mb > settings.MAX_FILE_SIZE_MB:
-        raise HTTPException(
-            status_code=400,
-            detail=f"File too large!"
-        )
+    validate_file(file)
     temp_path = os.path.join(settings.TEMP_PATH, file.filename)
     temp_path_excel = os.path.join(settings.TEMP_PATH, "result.xlsx")
     try:
---
+++ app/services/validator.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from fastapi import HTTPException, UploadFile
+from app.core.config import settings
+
+ALLOWRD_EXTENTIONS = [".txt", ".text", ".log", ".md"]
+
+def validate_file_extension(filename: str):
+    ext = Path(filename).suffix.lower()
+
+    if ext not in ALLOWRD_EXTENTIONS:
+        raise HTTPException(status_code=400, detail=f"File extansion {ext} doesn`t support")
+    
+def validate_file_size(file: UploadFile):
+    size = file.size / (1024*1024)
+    if size > settings.MAX_FILE_SIZE_MB:
+        raise HTTPException(status_code=400, detail=f"File`s size is too large. Max size is {settings.MAX_FILE_SIZE_MB} mb")
+    
+def validate_file(file: UploadFile):
+    if not file or not file.filename:
+        raise HTTPException(status_code=400, detail=f"File not exists")
+    
+    validate_file_extension(file.filename)
+    validate_file_size(file)
+
